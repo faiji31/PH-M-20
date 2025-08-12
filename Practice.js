21bfb404517a16cdfb5b49238c3eb15ebeed0fc3
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-12 21:12:09 +0600

    2nd commit

---
+++ Practice.js
@@ -0,0 +1,9 @@
+// Inch to Feet conversion with function
+function inchToFeet(inch){
+    const feet =inch/12;
+    return feet
+}
+const height = inchToFeet(73);
+console.log(height);
+const liamheight = inchToFeet(75);
+console.log(liamheight);
