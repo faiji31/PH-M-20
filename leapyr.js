0bae5a8ccc94f51058d8961d70352b511eaeb764
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-13 00:25:10 +0600

    7th commit

--- leapyr.js
+++ leapyr.js
@@ -11,3 +11,23 @@ const yr = isLeapyr(2021);
 console.log(yr);
 const yr1 = isLeapyr(2020);
 console.log(yr1);
+
+
+
+function isLeap(yr){
+    if (yr%100!==0 && yr%4===0){
+        return true;
+    }
+    else if (yr%400===0 && yr%100===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const liam = isLeap(2024);
+console.log(liam);
+const siam = isLeap(2043);
+console.log(siam);
+const jubu = isLeap(3031);
+console.log(jubu)
